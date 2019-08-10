9050429e6196f8a5126e4298d52e9beac6fc4fb0
takehiro_hidaka <gaku@gaku.local>
2019-08-10 11:45:13 +0900

    ReplaySubject

---
+++ 2.rxdart/ReplaySubject/1.basic/main.dart
@@ -0,0 +1,44 @@
+import 'package:rxdart/rxdart.dart';
+
+var subject = ReplaySubject<String>();
+
+void main() async {
+  // 購読
+  subject.listen((data){
+    print('検知1------');
+    print(data);
+  });
+
+  // 発行
+  subject.add("Item1");
+  subject.add("Item1");
+  subject.add("Item2");
+  subject.add("Item1");
+  
+  await Future.delayed(Duration(seconds: 5)); // このコードは検証用 5秒待つ
+  // 購読
+  subject.listen((data){
+    print('検知2------');
+    print(data);
+  });
+  await Future.delayed(Duration(seconds: 5)); // このコードは検証用 5秒待つ
+}
+
+/* output
+検知1------
+Item1
+検知1------
+Item1
+検知1------
+Item2
+検知1------
+Item1
+検知2------
+Item1
+検知2------
+Item1
+検知2------
+Item2
+検知2------
+Item1
+*/
